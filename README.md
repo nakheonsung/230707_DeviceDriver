617ec0247f31535abacd6f1d5a5eb43ef2481103
nakheon.sung <zlekflepqlf@gmail.com>
2023-07-07 10:09:14 +0900

    Setting first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## DeviceDriver KATA
+
+---
+
+DeviceDriver KATA
---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../230707_DeviceDriver/DeviceDriver.cpp"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
